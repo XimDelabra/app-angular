8a9b03710fb4ba286b5ea18b443af27b665c6690
Jesus Santiago <jesus.santiago.rv23@gmail.com>
2025-10-16 19:48:50 -0600

    Subida de componentes

---
+++ src/app/jsantiago/jsantiago.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jsantiago',
+  imports: [],
+  templateUrl: './jsantiago.component.html',
+  styleUrl: './jsantiago.component.css',
+})
+export class JsantiagoComponent { }
