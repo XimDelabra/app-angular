b917f4863a6cdec13992b39304cdc6e8f7b82e10
Miguel Camacho <cd8367741@gmail.com>
2025-10-30 02:07:30 -0600

    cambios

--- src/app/jsantiago/jsantiago.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-jsantiago',
-  imports: [],
-  templateUrl: './jsantiago.component.html',
-  styleUrl: './jsantiago.component.css',
-})
-export class JsantiagoComponent { }
--- src/app/lnunez/lnunez.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-lnunez',
-  imports: [],
-  templateUrl: './lnunez.component.html',
-  styleUrl: './lnunez.component.css',
-})
-export class LnunezComponent { }
