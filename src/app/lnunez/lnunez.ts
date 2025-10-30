318e16ab0b1c1d6d9e8eca5d00032abdcdbcb8bb
LeosterCode <164199518+LeosterCode@users.noreply.github.com>
2025-10-29 21:14:10 -0600

    Carga de componente lnunez

---
+++ src/app/lnunez/lnunez.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lnunez',
+  imports: [],
+  templateUrl: './lnunez.html',
+  styleUrl: './lnunez.css',
+})
+export class Lnunez { }
