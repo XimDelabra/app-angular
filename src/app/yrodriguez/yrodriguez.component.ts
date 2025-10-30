82413d0c9871e6bdb3d959426b0d05837bb5f597
Alberto Sosa <albertosslpz123@gmail.com>
2025-10-30 17:54:37 -0600

    Camnbios de Juan

--- src/app/yrodriguez/yrodriguez.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-yrodriguez',
-  imports: [],
-  templateUrl: './yrodriguez.component.html',
-  styleUrl: './yrodriguez.component.css',
-})
-export class YrodriguezComponent { }
