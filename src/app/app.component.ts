c753fe04a30265900f824abb7c458801ac54ef1e
Miguel Camacho <cd8367741@gmail.com>
2025-10-30 01:24:15 -0600

    Mi componente con mis datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Miguel } from './miguel/miguel'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Miguel],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-angular';
-}
+export class AppComponent { }
---
+++ src/app/miguel/miguel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-miguel',
+  imports: [],
+  templateUrl: './miguel.html',
+  styleUrl: './miguel.css'
+})
+export class Miguel {
+
+}
